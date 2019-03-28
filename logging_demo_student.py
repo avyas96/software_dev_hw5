8f39ed09dbdc5b704cac41174f7640fb0ad3a3d5
Samm <samm.i.amm.katcher@gmail.com>
2019-03-28 15:55:04 -0400

    updating info from build functions

--- logging_demo_student.py
+++ logging_demo_student.py
@@ -1,21 +1,19 @@
 import logging
 
 from student import Student
+from buildStudent import *
 
 
 def main():
-    print("creating student...")
-    john = Student("John", 1, 111222333, "212 West Ave", 20, "Computer Science")
+    john = createStudent("John", 1, 111222333, "212 West Ave", 20, "Computer Science")
     print("student named ", john.getName(), " with ID ", john.getAge())
-    print("Josh registered with the following phone number: ", john._getPhone_())
-    print("updating Josh's phone number:")
-    john._setPhone_(999888777)
+    #Update Phone number
+    updatePhone(john, 999888777)
 
     print("Printing John's full details")
-
     john.printStudentInfo()
     print()
-    #Note: The debug message and info message won't be visible on terminal. In order to view them you need to change the configuration.
+    # Note: The debug message and info message won't be visible on terminal. In order to view them you need to change the configuration.
     logging.basicConfig(level=logging.DEBUG)
     logging.debug('This is a debug message')
     logging.info('This is an info message')
@@ -24,7 +22,6 @@ def main():
     logging.error('This is an error message')
     logging.critical('This is a critical message')
 
-
     a = 5
     b = 0
 
@@ -32,6 +29,7 @@ def main():
         c = a / b
     except Exception as e:
         logging.error("Exception occurred", exc_info=True)
+
 
 if __name__ == "__main__":
     main()
