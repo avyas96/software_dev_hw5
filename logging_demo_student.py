f344587ee14f36e9806297b21098097036bf2463
avyas96 <vyasa@rpi.edu>
2019-03-28 15:32:45 -0400

    logging file added

---
+++ logging_demo_student.py
@@ -0,0 +1,37 @@
+import logging
+
+from student import Student
+
+
+def main():
+    print("creating student...")
+    john = Student("John", 1, 111222333, "212 West Ave", 20, "Computer Science")
+    print("student named ", john.getName(), " with ID ", john.getAge())
+    print("Josh registered with the following phone number: ", john._getPhone_())
+    print("updating Josh's phone number:")
+    john._setPhone_(999888777)
+
+    print("Printing John's full details")
+
+    john.printStudentInfo()
+    print()
+    #Note: The debug message and info message won't be visible on terminal. In order to view them you need to change the configuration.
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug('This is a debug message')
+    logging.info('This is an info message')
+
+    logging.warning('This is a warning message')
+    logging.error('This is an error message')
+    logging.critical('This is a critical message')
+
+
+    a = 5
+    b = 0
+
+    try:
+        c = a / b
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
+
+if __name__ == "__main__":
+    main()
